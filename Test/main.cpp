9f3bea6ca0a0642efe08b7f84f2893065d7ed1b9
HabibAroua <habibha.aroua82@gmail.com>
2018-01-27 00:46:14 +0100

    Calcule et affichage

---
+++ Test/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout << "Hello world!" << endl;
+    cout << "Donner un nombre 1 " <<endl;
+    cin >> a;
+    cout <<"Donner un nombre 2 "<<endl;
+    cin >> b;
+    int sum=a+b;
+    cout <<"La somme de "<< a << " et "<< b <<" ="<<sum <<endl;
+    return 0;
+}
---
+++ commit.sh
@@ -0,0 +1,4 @@
+`git remote add $1 https://github.com/HabibAroua/C_PLUS_PLUS_tp`
+`git pull $1 master`
+`git push $1 master`
+
