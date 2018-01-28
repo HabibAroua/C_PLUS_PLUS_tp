793c6f97cae7f54ebe2977d06cf001e1e8284b3e
HabibAroua <habibha.aroua82@gmail.com>
2018-01-28 01:22:33 +0100

    tableau

---
+++ Test/Test1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main1()
+{
+    //cout << "Hello world " <<endl;
+    int tab[20];
+    int i;
+    for (i=0 ; i<10 ; i++)
+    {
+         cout << "Tapez la case  "<<i<<endl;
+         cin >>tab[i];
+    }
+    return 0;
+}
