7d4f9074c70b5f89b759f108531146048d58fcd3
HabibAroua <habibha.aroua82@gmail.com>
2018-01-31 23:37:56 +0100

    delete pointer

--- Pointer/Pointer1/main.cpp
+++ Pointer/Pointer1/main.cpp
@@ -8,5 +8,13 @@ int main()
     *p = 5;
     *p=*p+1;
     cout << *p<<endl;
+
+    //Free
+    int *p1 = new int; // request memory
+    *p1 = 5; // store value
+    cout << *p1 << endl; // use value
+    delete p1; // free up the memory
+    cout << *p1 <<endl;
+
     return 0;
 }
