ac6f787e4bb45f0396d41dd7755e1625b115cf42
HabibAroua <habibha.aroua82@gmail.com>
2018-02-01 20:32:46 +0100

    declare function with parameter

--- Function/main.cpp
+++ Function/main.cpp
@@ -3,14 +3,23 @@
 using namespace std;
 
 void printSomething();
+void somme(int a , int b);
 
 int main()
 {
     printSomething();
+    cout <<endl;
+    somme(2,3);
     return 0;
 }
 
 void printSomething()
 {
   cout << "Hi there!";
+}
+
+void somme(int a , int b)
+{
+    int s=a+b;
+    cout<<"La somme est "<<s<<endl;
 }
