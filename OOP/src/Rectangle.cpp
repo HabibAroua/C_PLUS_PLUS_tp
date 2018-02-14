6d49167d7cf6959bf24908fb8c7c219001e976c0
HabibAroua <habibha.aroua82@gmail.com>
2018-02-14 19:43:43 +0100

    using getters and setters

--- OOP/src/Rectangle.cpp
+++ OOP/src/Rectangle.cpp
@@ -5,15 +5,35 @@ class Rectangle
 {
     private : int x;
     private : int y;
-    public :
-    void show()
-    {
 
-    }
     public :
     Rectangle(int x , int y)
     {
         this->x=x;
         this->y=y;
+    }
+
+    public :
+    int getX()
+    {
+        return this->x;
+    }
+
+    public :
+    int getY()
+    {
+        return this->y;
+    }
+
+    public :
+    void setX(int x)
+    {
+        this->x=x;
+    }
+
+    public :
+    void setY(int y)
+    {
+       this->y=y;
     }
 };
