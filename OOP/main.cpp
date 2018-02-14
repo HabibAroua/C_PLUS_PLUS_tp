eb6e43505619ab65a565ac22bb5bc9f497ca05ea
HabibAroua <habibha.aroua82@gmail.com>
2018-02-14 19:51:23 +0100

    apply the constructor in the main method

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "src/BankAccount.cpp"
+#include "src/Rectangle.cpp"
+
 using namespace std;
 
 class myClass
@@ -13,6 +15,14 @@ int main()
     test.sayHi();
     myClass myObj;
     myObj.name = "SoloLearn";
-    cout << myObj.name;
+    cout << myObj.name <<endl;
+    Rectangle r(1,2);
+    cout <<" le valeur de x est "<<r.getX()<<endl;
+    cout <<" le valeur de y est "<<r.getY()<<endl;
+    r.setX(3);
+    r.setY(-9);
+    cout <<" le valeur de x est "<<r.getX()<<endl;
+    cout <<" le valeur de y est "<<r.getY()<<endl;
+
     return 0;
 }
