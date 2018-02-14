a4b555c0d20394719dc6d0deb4d41d0e583dc468
HabibAroua <habibha.aroua82@gmail.com>
2018-02-14 19:33:39 +0100

    Encapsulation exemlple

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -2,9 +2,17 @@
 #include "src/BankAccount.cpp"
 using namespace std;
 
+class myClass
+{
+  public:
+    string name;
+};
 int main()
 {
     BankAccount test;
     test.sayHi();
+    myClass myObj;
+    myObj.name = "SoloLearn";
+    cout << myObj.name;
     return 0;
 }
