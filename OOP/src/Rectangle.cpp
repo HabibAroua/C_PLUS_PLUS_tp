19d8dde55933fdb38bf901850ec97ebb3530cf7a
HabibAroua <habibha.aroua82@gmail.com>
2018-02-14 19:28:49 +0100

    Create new class rectangle

---
+++ OOP/src/Rectangle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Rectangle
+{
+    private : int x;
+    private : int y;
+    public :
+    void show()
+    {
+
+    }
+    public :
+    Rectangle(int x , int y)
+    {
+        this->x=x;
+        this->y=y;
+    }
+};
