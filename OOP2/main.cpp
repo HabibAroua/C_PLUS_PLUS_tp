3076614ed0703c81a0456e5beb99cde3efb43c15
HabibAroua <habibha.aroua82@gmail.com>
2018-03-03 18:16:20 +0100

    Create class Cat

---
+++ OOP2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Cat
+{
+     private:
+     int age;
+     string name;
+
+     public:
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
